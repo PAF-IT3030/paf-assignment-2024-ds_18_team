3c39e578c32ff29b5676a335e22d37f998ae1f4b
wdewp <wdewpura@gmail.com>
2024-04-20 18:08:33 +0530

    Updated UserServiceImpl with @Autowired

--- backend/src/main/java/com/paf/socailfitnessapplication/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/paf/socailfitnessapplication/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    @Autowired
     private UserRepository userRepository;
 
     @Override
