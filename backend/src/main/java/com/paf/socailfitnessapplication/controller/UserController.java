d3d5f8e9f591755eeabf5739fd4f069ee8e3fad6
wdewp <wdewpura@gmail.com>
2024-04-20 17:58:51 +0530

    Updated UserController with @Autowired

--- backend/src/main/java/com/paf/socailfitnessapplication/controller/UserController.java
+++ backend/src/main/java/com/paf/socailfitnessapplication/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
     @PostMapping
