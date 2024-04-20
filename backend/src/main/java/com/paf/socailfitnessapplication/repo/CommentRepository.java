a5610e875e68dd16cd1f079f1898e86b09f94fd4
ovindu <ovindumandith@gmail.com>
2024-04-20 20:52:54 +0530

    added comment repo

--- backend/src/main/java/com/paf/socailfitnessapplication/repo/CommentRepository.java
+++ backend/src/main/java/com/paf/socailfitnessapplication/repo/CommentRepository.java
@@ -1,5 +1,13 @@
 package com.paf.socailfitnessapplication.repo;
 
-public class CommentRepository {
-    
+import com.paf.socailfitnessapplication.entity.Comment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends MongoRepository<Comment, String> {
+    List<Comment> findByPostId(String postId);
+
 }
