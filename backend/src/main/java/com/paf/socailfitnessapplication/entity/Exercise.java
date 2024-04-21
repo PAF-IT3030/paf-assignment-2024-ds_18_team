aaf9505c6cf6c841c4881a126b9b39072ddf046c
wdewp <wdewpura@gmail.com>
2024-04-21 07:17:37 +0530

    Implemented Exercise.java entity

---
+++ backend/src/main/java/com/paf/socailfitnessapplication/entity/Exercise.java
@@ -0,0 +1,17 @@
+package com.paf.socailfitnessapplication.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Exercise {
+
+    private String name;
+    private String sets;
+    private String reps;
+
+}
