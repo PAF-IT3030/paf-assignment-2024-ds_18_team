f63516e1f4ad5da5a6b37aedbf5741e232d51628
Sanduni Fdo <it21063596@my.sliit.lk>
2024-04-26 10:02:36 +0530

    Implemented MealPlanService

---
+++ backend/src/main/java/com/paf/socailfitnessapplication/service/MealPlanService.java
@@ -0,0 +1,21 @@
+package com.paf.socailfitnessapplication.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.paf.socailfitnessapplication.entity.MealPlan;
+
+
+
+public interface MealPlanService {
+    List<MealPlan> getAllMealPlans();
+
+    Optional<MealPlan> getMealPlanById(String mealPlanId);
+
+    MealPlan createMealPlan(MealPlan mealPlan);
+
+    MealPlan updatMealPlan(String mealPlanId, MealPlan mealPlan);
+
+    void deleteMealPlan(String mealPlanId);
+}
+
