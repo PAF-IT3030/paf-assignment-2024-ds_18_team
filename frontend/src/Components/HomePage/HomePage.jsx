c2acdb1595db219345263160e2f5a8f6de70f115
Sanduni Fdo <it21063596@my.sliit.lk>
2024-05-03 23:38:44 +0530

    Updated HomePage

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -12,16 +12,13 @@ const HomePage = () => {
 
       <Grid item xs={0} lg={2.5} className='hidden lg:block w-full relative'>
         <Navigation />
-
       </Grid>
 
       <Grid item xs={12} lg={6}
         className='px-5 lg:px-9 hidden lg:block w-full relative'>
-
         <Routes>
           <Route path="/profile/:id" element={<Profile />} />
         </Routes>
-
         <HomeSection />
 
       </Grid>
