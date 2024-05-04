87c20b2848e1b31ed0963b5f21f9d1398858e453
sulakna weerasinghe <87310753+sula15@users.noreply.github.com>
2024-05-04 16:51:45 +0530

    Added Filtering and sorting status updates

--- frontend/src/Components/StatusUpdate/StatusUpdatePage.jsx
+++ frontend/src/Components/StatusUpdate/StatusUpdatePage.jsx
@@ -1,6 +1,5 @@
-// StatusUpdatePage.js
 import React, { useState, useEffect } from 'react';
-import { IconButton, Typography, Button, Card, CardContent, CardActions } from '@mui/material';
+import { IconButton, Typography, Button, Card, CardContent, CardActions, Select, MenuItem, FormControl, InputLabel, Grid } from '@mui/material';
 import StatusUpdate from './StatusUpdate';
 import axios from 'axios';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -10,6 +9,10 @@ const StatusUpdatePage = () => {
   const [openStatusUpdate, setOpenStatusUpdate] = useState(false);
   const [statusUpdates, setStatusUpdates] = useState([]);
   const [editStatusUpdate, setEditStatusUpdate] = useState(null);
+  const [selectedMonth, setSelectedMonth] = useState('');
+  const [selectedYear, setSelectedYear] = useState('');
+  const [sortBy, setSortBy] = useState('timestamp'); // Default sorting by timestamp
+  const [sortOrder, setSortOrder] = useState('desc'); // Default sorting order
 
   const handleOpenStatusUpdate = () => setOpenStatusUpdate(true);
   const handleClose = () => {
@@ -29,6 +32,7 @@ const StatusUpdatePage = () => {
 
     fetchStatusUpdates();
   }, []);
+
   const handleEditStatusUpdate = async (id) => {
     try {
       const response = await axios.get(`http://localhost:8080/workout-status-updates/${id}`);
@@ -49,16 +53,104 @@ const StatusUpdatePage = () => {
     }
   };
 
+  const filterStatusUpdates = (updates) => {
+    if (selectedMonth === '' || selectedYear === '') {
+      return updates;
+    } else {
+      return updates.filter(update => {
+        const updateDate = new Date(update.timestamp);
+        return updateDate.getMonth() + 1 === selectedMonth && updateDate.getFullYear() === selectedYear;
+      });
+    }
+  };
+
+  // Sorting function for sorting by date and time
+  const sortStatusUpdates = (updates) => {
+    const sortedUpdates = [...updates].sort((a, b) => {
+      const dateA = new Date(a.timestamp);
+      const dateB = new Date(b.timestamp);
+      if (dateA.getTime() !== dateB.getTime()) {
+        return sortOrder === 'desc' ? dateB - dateA : dateA - dateB;
+      } else {
+        return sortOrder === 'desc' ? b.timestamp.localeCompare(a.timestamp) : a.timestamp.localeCompare(b.timestamp);
+      }
+    });
+    return sortedUpdates;
+  };
+  
   return (
     <div className="status-update-page">
       <div className="status-update-header">
         <Typography variant="h4" gutterBottom>Workout Status Updates</Typography>
-        <div style={{ marginLeft: 'auto' }}>
-          <Button onClick={handleOpenStatusUpdate} variant='contained' sx={{ borderRadius: "20px", bgcolor: '#20207D', color: '#fff', marginRight: '16px' }}>Add Status Update</Button>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={12} sm={6}>
+            <div style={{ display: 'flex' }}>
+              <FormControl sx={{ marginRight: '16px' }}>
+                <InputLabel id="month-select-label">Month</InputLabel>
+                <Select
+                  labelId="month-select-label"
+                  id="month-select"
+                  value={selectedMonth}
+                  onChange={(e) => setSelectedMonth(e.target.value)}
+                >
+                  <MenuItem value="">All</MenuItem>
+                  {/* Render month options */}
+                  {Array.from({ length: 12 }, (_, i) => (
+                    <MenuItem key={i + 1} value={i + 1}>{new Date(2000, i).toLocaleString('default', { month: 'long' })}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <FormControl>
+                <InputLabel id="year-select-label">Year</InputLabel>
+                <Select
+                  labelId="year-select-label"
+                  id="year-select"
+                  value={selectedYear}
+                  onChange={(e) => setSelectedYear(e.target.value)}
+                >
+                  <MenuItem value="">All</MenuItem>
+                  {/* Render year options */}
+                  {Array.from({ length: 10 }, (_, i) => (
+                    <MenuItem key={2022 + i} value={2022 + i}>{2022 + i}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <FormControl sx={{ marginRight: '16px' }}>
+                <InputLabel id="sort-by-label">Sort By</InputLabel>
+                <Select
+                  labelId="sort-by-label"
+                  id="sort-by-select"
+                  value={sortBy}
+                  onChange={(e) => setSortBy(e.target.value)}
+                >
+                  <MenuItem value="timestamp">Date</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl>
+                <InputLabel id="sort-order-label">Sort Order</InputLabel>
+                <Select
+                  labelId="sort-order-label"
+                  id="sort-order-select"
+                  value={sortOrder}
+                  onChange={(e) => setSortOrder(e.target.value)}
+                >
+                  <MenuItem value="asc">Ascending</MenuItem>
+                  <MenuItem value="desc">Descending</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+          </Grid>
+        </Grid>
+        <div style={{ marginTop: '16px', display: 'flex', justifyContent: 'flex-end' }}>
+          <Button onClick={handleOpenStatusUpdate} variant='contained' sx={{ borderRadius: "20px", bgcolor: '#20207D', color: '#fff' }}>Add Status Update</Button>
         </div>
       </div>
       <div className="status-update-list" style={{ marginTop: '16px' }}>
-        {statusUpdates.map((update, index) => (
+        {sortStatusUpdates(filterStatusUpdates(statusUpdates)).map((update, index) => (
           <div key={update.id} style={{ marginBottom: index < statusUpdates.length - 1 ? '16px' : 0 }}>
             <Card elevation={4}>
               <CardContent>
@@ -66,7 +158,7 @@ const StatusUpdatePage = () => {
                 <Typography variant="body1" gutterBottom><strong>Distance:</strong> {update.metrics.distance}</Typography>
                 <Typography variant="body1" gutterBottom><strong>Sets:</strong> {update.metrics.sets}</Typography>
                 <Typography variant="body1" gutterBottom><strong>Time:</strong> {update.metrics.time}</Typography>
-                <Typography variant="body2" gutterBottom><strong></strong> {new Date(update.timestamp).toLocaleString()}</Typography>
+                <Typography variant="body2" gutterBottom><strong>Date:</strong> {new Date(update.timestamp).toLocaleString()}</Typography>
               </CardContent>
               <CardActions>
                 <IconButton onClick={() => handleEditStatusUpdate(update.id)} style={{ color: '#1976D2' }}><EditIcon /></IconButton>
