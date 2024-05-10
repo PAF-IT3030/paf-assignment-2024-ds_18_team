11e183d6ce30e907aacb1bc3a26818c7c50f187b
ovindu <ovindumandith@gmail.com>
2024-05-10 09:35:21 +0530

    updated fronted o handle edit of posts

--- frontend/src/Components/HomeSection/EditPost.jsx
+++ frontend/src/Components/HomeSection/EditPost.jsx
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
 import { Button, TextField } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { updatePost } from "../Store/Action";
+import { uploadToS3 } from "../Config/awsS3";
 
-const EditPost = ({ initialCaption, initialImageUrl, onSubmit, onCancel }) => {
+const EditPost = ({
+  initialCaption,
+  initialImageUrl,
+  postId,
+  onSubmit,
+  onCancel,
+}) => {
+  const dispatch = useDispatch();
   const [caption, setCaption] = useState(initialCaption);
   const [image, setImage] = useState(null);
 
@@ -9,20 +19,21 @@ const EditPost = ({ initialCaption, initialImageUrl, onSubmit, onCancel }) => {
     setImage(e.target.files[0]);
   };
 
-  const handleSubmit = () => {
-    const formData = new FormData();
-    formData.append("file", image);
-    formData.append("upload_preset", "paf123");
-    fetch("https://api.cloudinary.com/v1_1/ddvmuwi3e/image/upload", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        const imageUrl = data.secure_url;
-        onSubmit(caption, imageUrl);
-      })
-      .catch((error) => console.error("Error uploading image:", error));
+  const handleSubmit = async () => {
+    try {
+      let updatedImageUrl = initialImageUrl; // Default to initial image URL
+      if (image) {
+        const fileName =
+          "paf_sfa_" + uuidv4() + "." + image.name.split(".").pop();
+        await uploadToS3(image, fileName);
+        updatedImageUrl = `https://${S3_BUCKET_NAME}.s3.${S3_BUCKET_REGION}.amazonaws.com/${fileName}`;
+      }
+
+      dispatch(updatePost(postId, { caption, imageUrl: updatedImageUrl }));
+      onSubmit(); // Close the edit form
+    } catch (error) {
+      console.error("Error updating post:", error);
+    }
   };
 
   return (
